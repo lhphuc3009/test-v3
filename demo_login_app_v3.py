c30c7e2053a0e0118fe9c118d87b09fc47f3fbe7
lhphuc3009 <lhphucgame3009@gmail.com>
2025-06-17 22:03:36 +0700

    demo_login_app_v3.py

--- demo_login_app_v3.py
+++ demo_login_app_v3.py
@@ -19,7 +19,7 @@ authenticator = stauth.Authenticate(
     config["cookie"]["expiry_days"]
 )
 
-name, auth_status, username = authenticator.login("Đăng nhập", "main")
+name, auth_status, username = authenticator.login("Login", "main")
 
 if auth_status is False:
     st.error("❌ Sai tài khoản hoặc mật khẩu.")
